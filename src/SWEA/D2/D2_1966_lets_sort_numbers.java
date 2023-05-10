93fd2c4da8edde2414055ef7fc63b08a24981efc
gogumaC(mac mini) <kimu2371@naver.com>
2023-05-10 19:07:35 +0900

    feat(SWEA): Solve D2_1966_lets_sort_numbers

---
+++ src/SWEA/D2/D2_1966_lets_sort_numbers.java
@@ -0,0 +1,31 @@
+package SWEA.D2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class D2_1966_lets_sort_numbers {
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            Arrays.sort(arr);
+            System.out.print("#"+test_case+" ");
+            for(int i=0;i<n;i++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.print("\n");
+
+        }
+    }
+}
